9b7d91432a39f687f3dbc465b3cc800d08541dcf
Paulo Oliveira <paulo.oliv03@gmail.com>
2020-08-14 16:02:18 -0300

    questao 5

---
+++ src/main/java/com/rd/treinamentodev/AvaliacaoSpringBoot/model/entity/ModuloEntity.java
@@ -0,0 +1,25 @@
+package com.rd.treinamentodev.AvaliacaoSpringBoot.model.entity;
+
+import lombok.Data;
+import org.hibernate.mapping.ToOne;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_modulo")
+@Data
+public class ModuloEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_modulo")
+//    @JsonIgnore
+    private Long idModulo;
+
+    @Column(name = "ds_nome")
+    private String nomeModulo;
+
+    @OneToOne
+    @JoinColumn(name = "id_instrutor")
+    private InstrutorEntity idinstrutor;
+}
